0a1cd28e0cdc4c5a6d43a895d97767322a068083
Jegath Pranava <jegathpranava@gmail.com>
2026-03-16 11:25:56 +0530

    ZeeStore: Fixed Render port binding by listening on 0.0.0.0 and respecting PORT variable

--- ChefSupply.API/Program.cs
+++ ChefSupply.API/Program.cs
@@ -146,4 +146,5 @@ app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
 
-app.Run();
+var port = Environment.GetEnvironmentVariable("PORT") ?? "80";
+app.Run($"http://0.0.0.0:{port}");
